0c03a7fd23b36a81c942732befae26e09f0ee2b4
Gustavo Mafra <gumafrak@gmail.com>
2020-10-29 20:53:37 -0300

    Correcao case 7

--- src/trabalho2/Main.java
+++ src/trabalho2/Main.java
@@ -5,50 +5,46 @@ import javax.swing.JOptionPane;
 public class Main {
 
 	public static void main(String[] args) {
-		int num[] = new int[20]; //Array 
+		int num[] = new int[20]; // Array
 		int opcao;
 		String ent;
 		int menor = 0;
 		int maior = 0;
-		int posMaior = 0; //Antes do switch-case
+		int posMaior = 0; // Antes do switch-case
 		int primos = 0;
-		
-		
+
 		for (int i = 0; i < 20; i++) {
 			ent = JOptionPane.showInputDialog("Insira um número:");
 			num[i] = Integer.parseInt(ent);
-			
+
 			if (i == 0) {
 				menor = num[i];
-				
+
 			} else if (num[i] < menor) {
 				menor = num[i];
 			}
-			
-			if (num[i] != 0 && num[i] != 1) {
+			if (num[i] == 2) {
+				primos++;
+			} else if (num[i] != 0 && num[i] != 1) {
 				for (int cont = num[i] - 1; cont > 1; cont--) {
 					if (num[i] % cont != 0) {
 						if (cont == 2) {
-							primos ++;
-						}
+							primos++;
+						} 
+					} else {
+						cont = 1;
 					}
 				}
 			}
-			
-			
+
 		}
-		
+
 		do {
-			ent = JOptionPane.showInputDialog("\n\t\tMenu\n\t1.Mostrar\n"
-					+ "\t2.Inverter\n"
-					+ "\t3.Ordenar Crescente\n"
-					+ "\t4.Ordenar Decrescente\n"
-					+ "\t5.Fatorial Menor\n"
-					+ "\t6.Posição Maior\n"
-					+ "\t7.Quantidade de Primos\n\n"
-					+ "\t9.Vazar");
+			ent = JOptionPane.showInputDialog("\n\t\tMenu\n\t1.Mostrar\n" + "\t2.Inverter\n" + "\t3.Ordenar Crescente\n"
+					+ "\t4.Ordenar Decrescente\n" + "\t5.Fatorial Menor\n" + "\t6.Posição Maior\n"
+					+ "\t7.Quantidade de Primos\n\n" + "\t9.Vazar");
 			opcao = Integer.parseInt(ent);
-			
+
 			switch (opcao) {
 			case 1:
 				for (int i = 0; i < 20; i++) {
@@ -56,14 +52,14 @@ public class Main {
 				}
 				break;
 			case 2:
-				for (int i = 0; i < 10; i++) { //10 para inverter uma vez só
+				for (int i = 0; i < 10; i++) { // 10 para inverter uma vez só
 					int aux = num[i];
-					num[i] = num[19 -i];
+					num[i] = num[19 - i];
 					num[19 - i] = aux;
 				}
 				break;
 			case 3:
-				for (int i =1; i <20; i++) {
+				for (int i = 1; i < 20; i++) {
 					for (int j = 0; j < i; j++) {
 						if (num[i] < num[j]) {
 							int aux = num[i];
@@ -74,7 +70,7 @@ public class Main {
 				}
 				break;
 			case 4:
-				for (int i =1; i <20; i++) {
+				for (int i = 1; i < 20; i++) {
 					for (int j = 0; j < i; j++) {
 						if (num[i] > num[j]) {
 							int aux = num[i];
@@ -82,19 +78,19 @@ public class Main {
 							num[j] = aux;
 						}
 					}
-				} 
+				}
 				break;
 			case 5:
 				int fatorial = menor;
-				for (int i = menor - 1; i > 1; i --) {
-					fatorial *= i; //Igual fatorial = fatorial *i;
-				} 
+				for (int i = menor - 1; i > 1; i--) {
+					fatorial *= i; // Igual fatorial = fatorial *i;
+				}
 				if (menor == 0) {
 					fatorial = 1;
 				}
 				System.out.println(fatorial);
 				break;
-			case 6: //Posição altera com mudança da ordem do array
+			case 6: // Posição altera com mudança da ordem do array
 				for (int i = 0; i < 20; i++) {
 					if (i == 0) {
 						maior = num[i];
@@ -104,7 +100,7 @@ public class Main {
 						posMaior = i;
 					}
 				}
-				
+
 				System.out.println(posMaior);
 				break;
 			case 7:
